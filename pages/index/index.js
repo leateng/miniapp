d387a63531fafb8e3af9580dac1637dfb1a81e3d
liteng <leateng@foxmail.com>
2019-05-28 00:20:20 +0800

    init commit

---
+++ pages/index/index.js
@@ -0,0 +1,79 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    value: ""
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  // event.detail 的值为当前选中项的索引
+  onTabChange: function(event) {
+    console.log(event.detail);
+    
+    if(event.detail == 2){
+      console.log("call phone");
+      wx.makePhoneCall({
+        phoneNumber: '17779336015',
+      })
+    }
+  },
+
+  // 刷新搜索框的值
+  onChange: function(event) {
+    this.setData({
+      searchString: event.detail
+    });
+  }, 
+
+  // 响应搜索按钮
+  onSearch: function (event){
+    console.log("searching: " + this.data.searchString);
+  }
+})
